950a06a7e48ca3b392e4b29e47448098e90609cd
myle-truong <56028880+myle-truong@users.noreply.github.com>
2019-11-26 16:48:26 +0100

    apdate

---
+++ app.js
@@ -0,0 +1,25 @@
+document.body.style.backgroundImage = "url('/img/bookshelf.jpg')";
+
+// intial values
+//const API_KEY = "AIzaSyCur9bR6CxPRGi1gC2ZkE6tG-b4i4FlCuY";
+//AIzaSyCur9bR6CxPRGi1gC2ZkE6tG-b4i4FlCuY
+
+// seclect element from the DOM
+const buttonElement = document.querySelector("#search");
+const inputElement = document.querySelector("#inputValue");
+const content = document.querySelector(".content");
+
+buttonElement.onclick = async function(event) {
+    const value = inputElement.value;
+
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${value}+inauthor:keyes&key=AIzaSyCur9bR6CxPRGi1gC2ZkE6tG-b4i4FlCuY`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+
+    content.innerHTML = `
+    <h2 class="title">Title: ${data.items[0].volumeInfo.title} </h2>
+    <h2>Author: ${data.items[0].volumeInfo.author} </h2>
+    <img src="http://books.google.com/books/content?id=${value}&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api">: ${data.items[0].volumeInfo.img}  `;
+};
